366819ab0fc2483a849a5a2ad12c3e05d58c30ee
Maciej Podgajny <maciej.podgajny@agitive.com>
2018-07-02 11:44:52 +0200

    Add app

---
+++ app/src/main/java/com/example/maciek/postviewer/PostListActivity.kt
@@ -0,0 +1,12 @@
+package com.example.maciek.postviewer
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PostListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.post_list_activity)
+    }
+}
